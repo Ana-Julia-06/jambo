c1a045f6664a55c7d85e456b203edfdfddc29a50
Ana Julia de Andrade Botelho Pinheiro <pinheiro.ana@academico.ifrn.edu.br>
2024-04-05 09:30:11 -0300

    show

--- loja/models/__init__.py
+++ loja/models/__init__.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 #acima são bibliotecas padrões necessárias do Django, e abaixo nossos models
+
+
 from .fabricante import Fabricante
 from .categoria import Categoria
+from .produto import Produto
--- loja/models/produto.py
+++ loja/models/produto.py
@@ -3,12 +3,13 @@ class Produto(models.Model):
     produto = models.CharField(null=False, max_length=100)
     destaque = models.BooleanField(default=True)
     promocao = models.BooleanField(default=True)
-    msgPromocao = models.CharField(null=True, max_length=100)
+    msgPromocao = models.CharField(null=True, max_length=100, blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
-    categoria = models.ForeignKey(Categoria, null=True, related_name='categoria', on_delete=models.SET_NULL)
-    fabricante = models.ForeignKey(Fabricante, null=True, related_name='fabricante', on_delete=models.SET_NULL)
+    categoria = models.ForeignKey(Categoria, null=True, related_name='categoria_do_produto', on_delete=models.SET_NULL)
+    fabricante = models.ForeignKey(Fabricante, null=True, related_name='fabricante_do_produto', on_delete=models.SET_NULL)
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.produto)

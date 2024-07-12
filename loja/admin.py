2e227a1a6034092db5c3b1d6ff5bf8eeeda67d23
Ana-Julia-06 <anajuliapinheiro06@gmail.com>
2024-07-12 10:18:39 -0300

    oiiiiii

--- loja/admin.py
+++ loja/admin.py
@@ -6,12 +6,18 @@ from .models import * #importa nossos models
 class FabricanteAdmin(admin.ModelAdmin):
     # Cria um filtro de hierarquia com datas
     date_hierarchy = 'criado_em'
+    search_fields = ('fabricante',)
+
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ('categoria',)
 
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
     list_display = ('produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
-    empty_value_display = 'Vazio'
+    empty_value_display = 'Sem promoção'
+    search_fields = ('produto',)
+    fields = ('produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria', 'fabricante',)
 
 admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do adm
-admin.site.register(Categoria)
+admin.site.register(Categoria, CategoriaAdmin)
 admin.site.register(Produto, ProdutoAdmin)
